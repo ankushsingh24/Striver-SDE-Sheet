bf61de236975903a6bf0c50604b603754e96b27b
Ankush Singh <83535682+ankushsingh24@users.noreply.github.com>
2023-07-29 21:28:53 +0530

    Create maximum-sum-combinations.cpp

---
+++ Heaps/maximum-sum-combinations.cpp
@@ -0,0 +1,30 @@
+vector<int> Solution::solve(vector<int> &A, vector<int> &B, int C) 
+{
+    int n = A.size();
+    
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    
+    priority_queue<pair<int, pair<int, int>>> pq;
+    vector<int> ans;
+    
+    for(int i=0 ; i<n ; i++)
+    {
+        pq.push({A[i] + B[n-1], {i, n-1}});
+    }    
+    
+    while(!pq.empty() && C--)
+    {
+        int val = pq.top().first;
+        int x = pq.top().second.first;
+        int y = pq.top().second.second;
+        pq.pop();
+        
+        ans.push_back(val);
+        
+        if(y != 0)
+            pq.push({A[x] + B[y-1], {x, y-1}});
+    }
+    
+    return ans;
+}
